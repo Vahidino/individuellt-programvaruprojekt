303b39b77e326b1059ccc85ed6926607bbe58cd0
Vahidino <vahc20@student.bth.se>
2022-10-09 20:54:18 +0200

    post sprint 2

--- corrector.py
+++ corrector.py
@@ -1,62 +1,53 @@
-def addNewLines(text, newLineSize):
+def add_new_lines(text, new_line_size):
     """lägger en ny rad efter varje punkt och komma om inte det finns redan en."""
-    
-    newText = ""
-    newLines = "\n" * newLineSize
-    
-    for i in range(len(text)):
+    new_text = ""
+    new_lines = "\n" * new_line_size
+    for i in range(enumerate(text)):
         if text[i] == "," or text[i] == "." or text[i] == "!" or text[i] == "?":
-            """if text[i] == "." is between two numbers dont ad \n
-               if text[i+1] != "int" and text[i-1] != int             
-            """
+            #if text[i+1] != "int" and text[i-1] != int
             if text[i+1] != "\n":
-                newText += text[i] + newLines
+                new_text += text[i] + new_lines
             else:
-                newText += text[i]
+                new_text += text[i]
         else:
-            newText += text[i]
-    return newText
+            new_text += text[i]
+    return new_text
 
 
-def addSpaceAfterComment(text, tabSize):
+def add_space_after_comment(text, tab_size):
     """lägger till ett mellanslag efter % om det inte finns redan ett mellanslag"""
 
-    newText = ""
-    tab = " " * tabSize
-    for i in range(len(text)):
+    new_text = ""
+    tab = " " * tab_size
+    for i in range(enumerate(text)):
         if text[i] == "%":
             if text[i+1] != " ":
-                newText += text[i] + tab
+                new_text += text[i] + tab
             else:
-                newText += text[i]
+                new_text += text[i]
         else:
-            newText += text[i]
-    return newText
+            new_text += text[i]
+    return new_text
 
-"""
-coding guidelines
-"""
-def addSpaceAfterOperator(text, newLineSize):
-        #Add new line after operators if there is no new line allready
-        start = text.find("\\begin{document}") # indexet för början av texten vi ska ändra
-        end = text.find("\\end{document}")  # indexet för slutet av texten vi ska ändra
+def add_space_after_operator(text, new_line_size):
+    """lägger till ett mellanslag efter operatorer om det inte finns redan ett mellanslag"""
+        #lägger till ny rad efter operatorer
+    start = text.find("\\begin{document}") # indexet för början av texten vi ska ändra
+    end = text.find("\\end{document}")  # indexet för slutet av texten vi ska ändra
 
-        textToEdit = text[start:end] # texten vi ska ändra
-        lines = textToEdit.splitlines() # dela upp texten i en lista med en sträng för varje rad
+    text_to_edit = text[start:end] # texten vi ska ändra
+    lines = text_to_edit.splitlines() # dela upp texten i en lista med en sträng för varje rad
 
-        newText = ""
+    new_text = ""
 
-        newLines = "\n" * newLineSize
+    new_lines = "\n" * new_line_size
 
-        for line in lines:
-            if "\\" in line:
+    for line in lines:
+        if "\\" in line:
             # if the next line is not a \n add a \n
-                nextLine = lines[lines.index(line) + 1]
-            if nextLine != "":
-                line += newLines
-        newText += line + "\n"
-    
-        newText = text[:start] + newText + text[end:]
-        return newText
-
-    
+            next_line = lines[lines.index(line) + 1]
+        if next_line != "":
+            line += new_lines
+        new_text += line + "\n"
+        new_text = text[:start] + new_text + text[end:]
+        return new_text
--- main.py
+++ main.py
@@ -1,48 +1,44 @@
-#LinterCleanter
+"""LinterCleanter"""
 import sys # vi använder detta för att kunna ta in parameter från när vi kör programmet
 import json
 import corrector
 
 def main():
-
-  if len(sys.argv) < 2: # Om vi har mindre än 2 argument, dvs. bara programnamnet
+    """Om vi har mindre än 2 argument, dvs. bara programnamnet"""
+if len(sys.argv) < 2:
     print("Usage: main.py <file> [options]")
     sys.exit(1)
 
-  if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+if sys.argv[1] == "--help" or sys.argv[1] == "-h":
     print("Usage: main.py [OPTION] [FILE]")
     print("  -l, --linebreaks\tAdd line breaks to the text")
     print("  -t, --tabs\t\tAdd tabs to the text")
     print("Example: main.py -l text.txt")
     sys.exit(0)
 
-  # json config file
-  jsonFile = open("config.json", "r").read()
-  jsonData = json.loads(jsonFile) # läser in json filen och gör om den till en dictionary
+# json config file
+json_file = open("config.json", "r", encoding="utf-8").read()
+json_data = json.loads(json_file) # läser in json filen och gör om den till en dictionary
 
- # file to edit
-  file = open(sys.argv[1], "r").read()
-  newLineSize = jsonData["newLineSize"]
-  tabSize = jsonData["tabSize"]
+# file to edit
+file = open(sys.argv[1], "r", encoding="utf-8").read()
+new_line_size = json_data["new_line_size"]
+tab_size = json_data["tab_size"]
 
   # implementera argv
-  """override 
-  sys.argv[2] = "new linesize"
-  sys.argv[3] = " new tabsize"
-  om ingen input normal values från json
-  [4] newlinesize = 4
-  problem:
-  hur ska man ställa inn tabs utan att skriva in linesize?
-  Recommend pylint to see errors
-  Using PEP8 as standard
-
-  """
-
+  #override config file
+  #sys.argv[2] = "new_line_size"
+  #sys.argv[3] = " new_tab_size"
+  #om ingen input normal values från json
+  #[4] newlinesize = 4
+  #problem:
+  #Using PEP8 as standard
+new_linesize = json_data["new_lineSize"]
   #kallar på funktionen i corrector.py
-  newText = corrector.addNewLines(file, newLineSize)
-  newText = corrector.addSpaceAfterComment(newText, tabSize)
-  #newText = corrector.addSpaceAfterOperator(newText, newLineSize)
-  print(newText)
+new_text = corrector.add_new_lines(file, new_line_size)
+new_text = corrector.add_space_after_comment(new_text, tab_size)
+new_text = corrector.add_space_after_operator(new_text, new_line_size)
+print(new_text)
 
 if __name__ == "__main__":
-  main()
+    main()
